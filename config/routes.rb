06cda9915c2211f94778a3dc8acd9794c436fff7
David Race <drace@abodo.com>
2018-02-07 10:05:12 -0600

    all but last section of chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get 'users/new'
+  # get 'users/new'
 
   #get 'static_pages/home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
