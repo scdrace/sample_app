681b8edf707f4a09ef8a9059da3442294439737e
David Race <drace@abodo.com>
2018-02-12 13:05:11 -0600

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    # puts params[:session]
+    # render "new"
+    # puts User.first.inspect
+    # user = User.first.update(password: "foobar")
+    # puts "PARAMS!!!: #{params[:sessions]}"
+    email = params[:session][:email].downcase
+    password = params[:session][:password].downcase
+    user = User.find_by(email: email)
+    if user && user.authenticate(password)
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,21 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  # get 'users/new'
-
-  #get 'static_pages/home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
+
+  # get  '/help', to: 'static_pages#help'
+  # get  '/about' => 'static_pages#about'
+  # get  '/contact' => 'static_pages#contact'
+  # get  '/jam/foo' => 'foo#help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
